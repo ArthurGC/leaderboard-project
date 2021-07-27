13a783df8d464eac7ceab85e0ed29b0b03dba7c3
arthurGC <aguzmanc@unsa.edu.pe>
2021-07-26 20:45:17 -0500

    Add content to html file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import { plainHTML } from './modules/structure';
 import './styles.scss';
+
+let root = document.querySelector('#root');
+
+document.addEventListener('DOMContentLoaded', () => {
+    root.insertAdjacentHTML('beforeend', plainHTML);
+})
