b88fc1b05a0b250668e75d20983eea42d01dcf00
arthurGC <aguzmanc@unsa.edu.pe>
2021-07-27 09:39:06 -0500

    Fix html template

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import { plainHTML } from './modules/structure.js';
 import './styles.scss';
-
-const root = document.querySelector('#root');
-
-document.addEventListener('DOMContentLoaded', () => {
-  root.insertAdjacentHTML('beforeend', plainHTML);
-});
