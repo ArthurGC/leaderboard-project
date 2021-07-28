0fe4a737acf854233ab9cdca0564d56687a5f198
arthurGC <aguzmanc@unsa.edu.pe>
2021-07-27 22:58:25 -0500

    Add addScore function

--- src/modules/scores.js
+++ src/modules/scores.js
@@ -11,3 +11,26 @@ export async function getScores () {
     const scores = await response.json();
     return scores.result;
 }
+
+export async function addScore () {
+
+    let name = document.querySelector('#name').value;
+    let score = document.querySelector('#score').value;
+    let newScore = { 
+        user: name,
+        score: parseInt(score, 10) 
+    }
+    
+    const response = await fetch(scoreURL, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+        body: JSON.stringify(newScore),
+    });
+    name = '';
+    score = '';
+
+    const status = await response.json();
+    return status;
+}
