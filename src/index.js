19c3f7b2409c186c6c331de1aed043629d8acdeb
arthurGC <aguzmanc@unsa.edu.pe>
2021-07-27 22:59:13 -0500

    Add functionality to submit and refresh buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './styles.scss';
+import { createTable } from './modules/create';
+import { addScore, getScores } from './modules/scores';
+
+let submit = document.querySelector('.submit');
+let refresh = document.querySelector('.refresh');
+
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    addScore();
+    getScores().then(listScore => {
+        createTable(listScore);
+     })
+});
+
+refresh.addEventListener('click', () => {
+    getScores().then(listScore => {
+       createTable(listScore);
+    })
+})
