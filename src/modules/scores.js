5afb759a60dde7cadae4a00729688d2f650bb81c
arthurGC <aguzmanc@unsa.edu.pe>
2021-07-27 22:58:07 -0500

    Add getScores function

--- src/modules/scores.js
+++ src/modules/scores.js
@@ -1,1 +1,13 @@
 const scoreURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/tvIs41iLCOTgjlAGQnjJ/scores/';
+
+export async function getScores () {
+    const response = await fetch(scoreURL);
+
+    if (!response.ok) {
+        const message = `An error has occured: ${response.status}`;
+        throw new Error(message);
+    };
+
+    const scores = await response.json();
+    return scores.result;
+}
